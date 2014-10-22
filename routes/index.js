82a6eacc815f5cd5c8863fe9b35cd1afefcd4402
Garrett McCullough <gwmccull@yahoo.com>
2014-10-21 23:19:36 -0700

    Fixed title on home page

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ module.exports = function(passport) {
 
 	/* GET Home Page */
 	router.get('/home', isAuthenticated, function (req, res) {
-		res.render('home', { user: req.user });
+		res.render('home', { user: req.user, title: 'Home' });
 		//res.json(req.user);
 	});
 
