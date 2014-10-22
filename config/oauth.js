da53830a79932cac9352f5e202719b0a1dba8f15
Garrett McCullough <gwmccull@yahoo.com>
2014-10-21 21:31:50 -0700

    removed port from config file

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ var ids = {
 	facebook: {
 		clientID: '522978111171437',
 		clientSecret: '815370519ad9d14f4c4b9aa9d17e047e',
-		callbackURL: 'http://passportjs-facebook-demo.herokuapp.com:3000/auth/facebook/callback'
+		callbackURL: 'http://passportjs-facebook-demo.herokuapp.com/auth/facebook/callback'
 	}
 };
 
